45fb0c7e90aeb950409bdb0ef68e71f5fb344db0
josoya <thdi4564@nate.com>
2023-04-24 16:30:20 +0900

    spring security 셋팅

--- src/main/java/com/fourfree/new_efarm/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/fourfree/new_efarm/common/security/JwtAuthenticationFilter.java
@@ -20,23 +20,23 @@ import java.io.IOException;
  * 6  연락처     : 010-8299-5258
  */
 
-public class JwtAuthenticationFilter extends GenericFilterBean {
+//public class JwtAuthenticationFilter extends GenericFilterBean {
 
-    private JwtTokenProvider jwtTokenProvider;
-
-    // Jwt Provier 주입
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    // Request로 들어오는 Jwt Token의 유효성을 검증(jwtTokenProvider.validateToken)하는 filter를 filterChain에 등록합니다.
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(auth);
-        }
-        filterChain.doFilter(request, response);
-    }
-}
+//    private JwtTokenProvider jwtTokenProvider;
+//
+//    // Jwt Provier 주입
+//    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+//        this.jwtTokenProvider = jwtTokenProvider;
+//    }
+//
+//    // Request로 들어오는 Jwt Token의 유효성을 검증(jwtTokenProvider.validateToken)하는 filter를 filterChain에 등록합니다.
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+//        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+//        if (token != null && jwtTokenProvider.validateToken(token)) {
+//            Authentication auth = jwtTokenProvider.getAuthentication(token);
+//            SecurityContextHolder.getContext().setAuthentication(auth);
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//}
